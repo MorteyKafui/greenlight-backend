35dbc857388c699e5d3da319fa650b36e46f8421
Mortey Kafui <morteykafui@gmail.com>
2025-03-28 09:13:40 +0000

    add custom responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.kafui.net/internal/data"
 )
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of a movie %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
