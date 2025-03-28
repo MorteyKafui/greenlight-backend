796f04e0938fea0ab9821b9ea5c10c6fedf4160d
Mortey Kafui <morteykafui@gmail.com>
2025-03-28 20:55:52 +0000

    add migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -38,7 +38,7 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight", "PostgreSQl DSN")
-	// flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT"), "PostgreSQl DSN")
+	// flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQl DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdelTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
